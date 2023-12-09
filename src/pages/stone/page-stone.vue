306ca7ec83b0cf06b4e7fef5b342cc09d82fb9ed
hyongnim <409337645@qq.com>
2023-12-09 12:19:53 +0800

    stone list

--- src/pages/stone/page-stone.vue
+++ src/pages/stone/page-stone.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="q-pa-md">
-    <div class="row">
-      <div class="col-12 col-sm-6 col-lg-4 pa-2">
+    <div class="row" v-if="!rows">
+      <div v-for="i in 2" :key="i" class="col-12 col-sm-6 col-lg-4 pa-2">
+        <q-card flat>
+          <q-skeleton height="240px" square />
+        </q-card>
+      </div>
+    </div>
+    <div class="ta-c" v-else-if="!rows.length">
+      <div class="pa-8">
+        <img src="/img/stone/stone-empty.svg" width="280" />
+        <div class="fz-15 op-9">As empty as a cloudless sky</div>
+      </div>
+    </div>
+    <div class="row" v-else>
+      <div class="col-12 col-sm-6 col-lg-4 pa-2" v-for="it in rows" :key="it.id">
         <img src="img/stone/stone-head.png" height="40px" class="d-b" />
         <div class="bg-card-1 stone-card pa-4 pos-r" style="top: -13px">
           <div class="pos-a right-0 top-0 m-3 pa-1 hover-1">
-            <img src="img/stone/stone-edit.svg" width="20" />
+            <img src="/img/stone/stone-edit.svg" width="20" />
           </div>
           <div class="al-c">
             <div class="stone-cover">
-              <q-img src="https://qs3.4everland.store/logos/preact.svg" width="110px" :ratio="1" />
+              <q-img :src="it.avatar" width="110px" :ratio="1" />
             </div>
             <div class="ml-4">
               <p v-for="i in 3" :key="i">
@@ -19,8 +32,8 @@
             </div>
           </div>
           <div class="mt-4">
-            <p class="fw-b fz-20">This is name</p>
-            <p class="mt-2 op-8">Updated today</p>
+            <p class="fw-b fz-20">{{ it.stoneName }}</p>
+            <p class="mt-2 op-8">{{ it.bio }}</p>
           </div>
           <div class="mt-3 al-c">
             <q-btn color="primary" class="flex-1" rounded>Stone Page</q-btn>
@@ -36,9 +49,22 @@
 export default {
   data() {
     return {
-      loading: false,
-      rows: [],
+      rows: null,
     };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    async getList() {
+      try {
+        const { data } = await this.$http.get("/stone");
+        console.log(data);
+        this.rows = data;
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
