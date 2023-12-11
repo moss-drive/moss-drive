973db30792d650889d712f1fdf19996c42317896
yepCing <yepCing@gmail.com>
2023-12-11 14:01:50 +0800

    chore: update recharge

---
+++ src/pages/resource/componets/resource-progress.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="resource-info mb-2 al-c space-btw">
+      <div>{{ name }}</div>
+      <div class="fz-12 resource-usage">
+        <span class="resource-num fz-20" :style="{ color: color }">500</span>
+        <span class="unit">GB</span>
+        <span>/</span>
+        <span>5GB</span>
+      </div>
+    </div>
+    <q-linear-progress
+      :value="0.4"
+      size="8px"
+      rounded
+      color="color"
+      style="background: #334155"
+      :style="{
+        '--color': color,
+      }"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      default: "#57B9BC",
+    },
+    used: {
+      type: String,
+      default: "123123123",
+    },
+    total: {},
+  },
+  computed() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.text-color {
+  color: var(--color) !important;
+}
+
+.resource-info {
+  color: #cbd5e1;
+  .resource-usage {
+    > span {
+      margin-left: 2px;
+    }
+    .resource-num {
+      font-family: "DIN Alternate";
+    }
+  }
+}
+</style>
--- src/pages/resource/page-resource.vue
+++ src/pages/resource/page-resource.vue
@@ -1,33 +1,65 @@
-<style lang="scss">
-.q-linear-progress__track--dark {
-  border-radius: 20px;
-  background: var(--q-primary);
-  opacity: 1;
-}
-.q-linear-progress__model {
-  border-radius: 20px;
-}
-</style>
-
 <template>
   <div class="q-pa-md">
-    <div class="bg-card-1 pa-4">
-      <div class="al-c">
-        <span class="title">IPFS Storage</span>
-        <span class="ml-auto fz-13">
-          <!-- Total 120GB July 3,2022 -->
-          {{ usage.used }}/{{ usage.total }}
-        </span>
+    <resource-notice></resource-notice>
+    <div class="resource-plate al-c">
+      <div class="land-container">
+        <div class="fz-20 fw-b">Balance</div>
+        <div class="my-4 al-c space-btw">
+          <div class="land-content h-flex">
+            <div class="land-content-top">
+              <span class="land-amount">23,233</span>
+              <span class="fz-14">k</span>
+              <span class="fw-b fz-20 ml-4">LAND</span>
+            </div>
+
+            <div class="land-content-bottom mt-1">≈100USD</div>
+          </div>
+          <div class="conversion-btn cursor-p">Conversion</div>
+        </div>
+
+        <div class="descrition">
+          <span class="cursor-p">What's LAND?</span>
+          <span class="px-2">|</span>
+          <span class="cursor-p">Pricing</span>
+        </div>
       </div>
-      <div class="mt-4">
-        <q-linear-progress
-          :value="0"
-          :buffer="usage.perc"
-          size="10px"
-          rounded
-          color="accent"
-          style="background: #334155"
-        />
+      <div class="usage-container ml-6">
+        <div class="al-c">
+          <span class="fz-20 fw-b">Usage & Free Resource</span>
+          <img class="ml-1 cursor-p" width="24" src="/img/resource/help.svg" alt="" />
+        </div>
+
+        <template v-for="item in list" :key="item.name">
+          <resource-progress class="mt-4" :name="item.name" :color="item.color"></resource-progress>
+        </template>
+      </div>
+    </div>
+
+    <div class="recharge-container my-6">
+      <div class="deposit-container">
+        <div class="fz-20 fw-b mb-4">Deposit</div>
+        <div class="deposite-section">
+          <div class="al-c recharge-input">
+            <input maxlength="8" class="r-ipt flex-1" v-model="amount" type="text" />
+            <span class="num">,000,000</span>
+            <span class="land-text fz-14">LAND</span>
+          </div>
+          <div class="mt-1 fz-12 land-to-usd">1,000,000LAND=1USD</div>
+        </div>
+
+        <div class="fz-20 fw-b mb-4">Calculator</div>
+
+        <div class="al-c">
+          <resource-count-row
+            v-for="item in resourceTagsConfig"
+            :key="item.name"
+            :tags="item.tags"
+            :unit="item.unit"
+            :items="item.items"
+            :name="item.name"
+            @countPrice="countPrice"
+          ></resource-count-row>
+        </div>
       </div>
     </div>
   </div>
@@ -35,12 +67,189 @@
 
 <script>
 import { mapState } from "vuex";
-
+import ResourceNotice from "./componets/resource-notice.vue";
+import ResourceProgress from "./componets/resource-progress.vue";
+import ResourceCountRow from "./componets/resource-count-row.vue";
 export default {
+  data() {
+    return {
+      list: [
+        { name: "Storage (IPFS)", used: "2312312312", total: "23213211", color: "#57B9BC" },
+        { name: "BandWidth", used: "2312312312", total: "23213211", color: "#F3CC5C" },
+      ],
+      amount: "",
+      resourceTagsConfig: [
+        {
+          name: "IPFS Storage",
+          tags: [
+            {
+              text: "30 GB",
+              value: 30 * Math.pow(1024, 3),
+            },
+            {
+              text: "80 GB",
+              value: 80 * Math.pow(1024, 3),
+            },
+            {
+              text: "500 GB",
+              value: 500 * Math.pow(1024, 3),
+            },
+          ],
+          unit: Math.pow(1024, 3),
+          items: [
+            {
+              text: "MB",
+              value: Math.pow(1024, 2),
+            },
+            { text: "GB", value: Math.pow(1024, 3) },
+            {
+              text: "TB",
+              value: Math.pow(1024, 4),
+            },
+          ],
+        },
+        {
+          name: "IPFS Storage Days",
+          tags: [
+            {
+              text: "180 Days",
+              value: 180 * 86400,
+            },
+            {
+              text: "365 Days",
+              value: 365 * 86400,
+            },
+            {
+              text: "1096 Days",
+              value: 1096 * 86400,
+            },
+          ],
+          unit: 86400,
+          items: [{ text: "Days" }],
+        },
+        {
+          name: "BindWidth",
+          tags: [
+            {
+              text: "500 MB",
+              value: 500 * Math.pow(1024, 2),
+            },
+            {
+              text: "30 GB",
+              value: 30 * Math.pow(1024, 3),
+            },
+            {
+              text: "100 GB",
+              value: 100 * Math.pow(1024, 3),
+            },
+          ],
+          unit: Math.pow(1024, 3),
+          items: [
+            {
+              text: "MB",
+              value: Math.pow(1024, 2),
+            },
+            { text: "GB", value: Math.pow(1024, 3) },
+            {
+              text: "TB",
+              value: Math.pow(1024, 4),
+            },
+          ],
+        },
+      ],
+    };
+  },
   computed: {
     ...mapState({
       usage: (s) => s.usageInfo,
     }),
   },
+  methods: {
+    countPrice() {
+      console.log(2);
+    },
+  },
+  components: {
+    ResourceNotice,
+    ResourceProgress,
+    ResourceCountRow,
+  },
 };
 </script>
+<style lang="scss" scoped>
+.land-container,
+.usage-container {
+  flex: 1;
+  border-radius: 16px;
+  background: #0f172a;
+  padding: 16px;
+  color: #fff;
+  .land-content {
+    .land-content-top {
+      height: 46px; /* 115% */
+      .land-amount {
+        font-size: 40px;
+        font-family: "DIN Alternate";
+        margin-right: 2px;
+      }
+    }
+    .land-content-bottom {
+      color: #cbd5e1;
+      font-size: 14px;
+    }
+  }
+  .conversion-btn {
+    padding: 12px 16px;
+    border-radius: 4px;
+    border: 1px solid #1eefa4;
+    color: #1eefa4;
+  }
+
+  .descrition {
+    color: #cbd5e1;
+    font-size: 12px;
+  }
+}
+
+.recharge-container {
+  background: #0f172a;
+  border-radius: 16px;
+  overflow: hidden;
+  .deposit-container {
+    padding: 16px 16px 24px 16px;
+    border-bottom: 1px solid #334155;
+
+    .recharge-input {
+      border-radius: 4px;
+      border: 1px solid #334155;
+      .r-ipt {
+        font-size: 20px;
+        font-family: "DIN Alternate";
+        text-indent: 16px;
+        text-align: right;
+        background: #0f172a;
+        color: #1eefa4;
+      }
+      .num {
+        font-family: "DIN Alternate";
+        font-size: 20px;
+        padding-right: 16px;
+        color: #cbd5e1;
+      }
+      .land-text {
+        display: inline-block;
+        width: 160px;
+        color: #cbd5e1;
+        text-align: center;
+        font-size: 14px;
+        padding: 16px 0px;
+        background: #1e293b;
+      }
+    }
+
+    .land-to-usd {
+      color: #cbd5e1;
+    }
+  }
+}
+</style>
