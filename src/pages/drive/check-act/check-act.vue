4d8272bc06a8ba62de1d660df82c10d010b02ba8
hqs <imhuangqs@gmail.com>
2023-12-25 16:56:18 +0800

    stone

--- src/pages/drive/check-act/check-act.vue
+++ src/pages/drive/check-act/check-act.vue
@@ -105,9 +105,9 @@ export default {
       const item = this.checkItem;
       if (name == "publish") {
         if (this.stoneNum) {
-          return this.$alert(
-            "In order to enable creators to concentrate more on managing their Stones effectively, the current version only permits users to create ONE Stone. We will introduce the option to create multiple Stones in future releases."
-          );
+          // return this.$alert(
+          //   "In order to enable creators to concentrate more on managing their Stones effectively, the current version only permits users to create ONE Stone. We will introduce the option to create multiple Stones in future releases."
+          // );
         }
         if (localStorage.bindX) {
           localStorage.bindX = "";
