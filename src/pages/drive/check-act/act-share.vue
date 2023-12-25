7e24809edafdb14cfe73c8681e02ba0b60c44606
hqs <imhuangqs@gmail.com>
2023-12-25 17:55:31 +0800

    initForm

--- src/pages/drive/check-act/act-share.vue
+++ src/pages/drive/check-act/act-share.vue
@@ -99,7 +99,10 @@ const dayOpts = [
     txt: "Permanent",
   },
 ];
-
+const initForm = {
+  type: "DAY_1",
+  code: "",
+};
 export default {
   props: {
     checkList: Array,
@@ -111,8 +114,7 @@ export default {
       dayOpts,
       codeType: "auto",
       form: {
-        type: "DAY_1",
-        code: "",
+        ...initForm,
       },
       resData: null,
     };
@@ -129,6 +131,9 @@ export default {
     showPop(val) {
       if (val) {
         this.resData = null;
+        this.form = {
+          ...initForm,
+        };
       }
     },
   },
