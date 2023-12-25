827c15d6a99eda71e18357837b48132352b0b36a
hqs <imhuangqs@gmail.com>
2023-12-25 17:26:21 +0800

    media pre

--- src/pages/drive/check-act/act-stone.vue
+++ src/pages/drive/check-act/act-stone.vue
@@ -167,6 +167,7 @@
 import { mapState } from "vuex";
 import { MossHub } from "../../../utils/moss-hub";
 const { VITE_MEDIA_PRE } = import.meta.env;
+console.log(VITE_MEDIA_PRE);
 
 const initForm = {
   stoneName: "",
