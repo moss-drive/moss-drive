15974dd1697516e3748fa794a197f98b267b3449
hqs <imhuangqs@gmail.com>
2024-01-08 11:21:41 +0800

    d

--- src/components/login/login-popup.vue
+++ src/components/login/login-popup.vue
@@ -4,7 +4,6 @@
       <div class="pos-a right-0 top-0 m-2">
         <q-btn icon="close" flat round dense v-close-popup />
       </div>
-      <!-- <q-card-section> Sign In </q-card-section> -->
       <div class="fz-18 fw-b mb-2">SIGN IN</div>
       <q-card-section>
         <wallet-connect keep @login="showPop = false" :noInvited="noInvited"></wallet-connect>
