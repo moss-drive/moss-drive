90e7d6b0710c5f8e01c66ba4e34299241e07c361
yepCing <yepCing@gmail.com>
2024-01-15 17:16:58 +0800

    chore: update taskwrap

--- src/utils/taskWrap.js
+++ src/utils/taskWrap.js
@@ -61,72 +61,7 @@ export class UploadTask {
     this.status = 0;
   }
 }
-export class DeleteTask {
-  static id = 0;
-  constructor(s3, param) {
-    this.s3 = s3;
-    this.param = param;
-    this.id = DeleteTask.id++;
-    this.status = 0; // pre delete
-    this.deleteCount = 0;
-  }
-  async start() {
-    try {
-      if (this.status !== 0 && this.status !== 1) return;
-      this.status = 1; // deleting
-      const listResultStream = this.s3m.extensions.listObjectsV2WithMetadataQuery(
-        this.param.Bucket,
-        this.param.Prefix,
-        "",
-        "",
-        100,
-        ""
-      );
-      const listResult = await new Promise((resolve, reject) => {
-        listResultStream.on("data", resolve);
-        listResultStream.on("error", reject);
-      });
-      if (!listResult.objects) {
-        this.curFiles = [];
-      } else {
-        this.curFiles = listResult.objects.map((it) => {
-          return it.name;
-        });
-      }
-      if (this.curFiles.length && this.status == 1) {
-        await new Promise((resolve, reject) => {
-          this.s3m.removeObjects(this.param.Bucket, this.curFiles, (err) => {
-            if (err) reject(err);
-            else resolve();
-          });
-        });
 
-        for (let i = 0; i < listResult.objects.length; i++) {
-          this.deleteCount += 1;
-          await Vue.prototype.$sleep(20);
-        }
-        await this.startTasks();
-      } else if (!this.curFiles.length) {
-        this.status = 3; // success
-      } else {
-        console.log("here");
-      }
-    } catch (error) {
-      this.status = 4;
-      console.log(error);
-      throw new Error(error.code);
-    }
-  }
-  cancel() {
-    this.status = 2; //stop
-  }
-  updateS3mInstance(s3m) {
-    this.s3m = s3m;
-  }
-  retryTasks() {
-    this.status = 0; // retry
-  }
-}
 export class TaskWrapper {
   tasks = [];
   limit;
